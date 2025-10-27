7ad67ae530953028c1354d1d3fb8825d74515223
Ismael Bonato <ismaelbonato@gmail.com>
2025-10-27 18:09:04 +0000

    Implement RetweetCollection and associated tests for basic functionality

---
+++ Chapter_03/Version_02/RetweetCollection.h
@@ -0,0 +1,8 @@
+#pragma once
+
+class RetweetCollection
+{
+public:
+    bool isEmpty() const { return 0 == size(); }
+    unsigned int size() const { return 0; }
+};
---
+++ Chapter_03/Version_04/RetweetCollection.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "Tweet.h"
+
+class RetweetCollection
+{
+public:
+    RetweetCollection()
+        : size_(0)
+    {}
+
+    bool isEmpty() const { return 0 == size(); }
+
+    unsigned int size() const { return size_; }
+
+    void add(const Tweet &tweet) { size_ = 1; }
+
+private:
+    unsigned int size_;
+};
