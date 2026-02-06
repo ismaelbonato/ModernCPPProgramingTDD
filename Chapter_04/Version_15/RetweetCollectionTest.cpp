c690fee6f1074e342f1619d5efc296962e31debb
Ismael Bonato <ismaelbonato@gmail.com>
2026-02-06 13:11:38 +0000

    feat: add exercise 14 and 15 from chapter 4

---
+++ Chapter_04/Version_15/RetweetCollectionTest.cpp
@@ -0,0 +1,82 @@
+#include "RetweetCollection.h"
+#include "Tweet.h"
+#include "gmock/gmock.h"
+
+using namespace ::testing;
+
+class ARetweetCollection : public Test
+{
+public:
+    RetweetCollection collection;
+};
+
+class ARetweetCollectionWithOneTweet : public Test
+{
+public:
+    RetweetCollection collection;
+    void SetUp() override { collection.add(Tweet()); }
+};
+
+TEST_F(ARetweetCollection, IgnoresDuplicateTweetAdded)
+{
+    Tweet tweet("msg", "@user");
+    Tweet duplicate(tweet);
+    collection.add(tweet);
+    collection.add(duplicate);
+
+    ASSERT_THAT(collection.size(), Eq(1u));
+}
+
+TEST_F(ARetweetCollectionWithOneTweet, IsNoLongerEmpty)
+{
+    ASSERT_FALSE(collection.isEmpty());
+}
+
+TEST_F(ARetweetCollectionWithOneTweet, HasSizeOfOne)
+{
+    ASSERT_THAT(collection.size(), Eq(1u));
+}
+
+MATCHER_P(HasSize, expected, "")
+{
+    return arg.size() == expected && arg.isEmpty() == (0 == expected);
+}
+
+TEST_F(ARetweetCollection, IsEmptyWhenCreated)
+{
+    ASSERT_TRUE(collection.isEmpty());
+}
+
+TEST_F(ARetweetCollection, HasSizeZeroWhenCreated)
+{
+    ASSERT_THAT(collection.size(), Eq(0u));
+}
+
+TEST_F(ARetweetCollection, HasSizeOfOneAfterTweetAdded)
+{
+    collection.add(Tweet());
+
+    ASSERT_THAT(collection.size(), Eq(1u));
+}
+
+TEST_F(ARetweetCollection, DecreasesSizeAfterRemovingTweet)
+{
+    collection.add(Tweet());
+
+    collection.remove(Tweet());
+
+    ASSERT_THAT(collection, HasSize(0u));
+}
+
+TEST_F(ARetweetCollection, IsEmptyWhenItsSizeIsZero)
+{
+    ASSERT_THAT(collection.size(), Eq(0u));
+    ASSERT_TRUE(collection.isEmpty());
+}
+
+TEST_F(ARetweetCollection, IsNotEmptyWhenItsSizeIsNonZero)
+{
+    collection.add(Tweet());
+    ASSERT_THAT(collection.size(), Gt(0u));
+    ASSERT_FALSE(collection.isEmpty());
+}
